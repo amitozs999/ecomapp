63c782bb904086765a8b8ac25e65f0a4d08575bd
amitozs999 <ekas0001@gmail.com>
2023-09-29 19:04:58 +0530

    user can add and delete products in wishlist

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -11,8 +11,13 @@ import StarRating from "react-star-ratings";
 import RatingModal from "../modal/RatingModal";
 import { showAverage } from "../../functions/rating";
 
+import { addToWishlist } from "../../functions/user";
+
 import _ from "lodash";
 import { useSelector, useDispatch } from "react-redux";
+
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 const { TabPane } = Tabs;
 
@@ -26,6 +31,8 @@ const SingleProduct = ({ product, onStarClick, star }) => {
 
   const { user, cart } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
+
+  let history = useHistory();
 
   const { title, images, description, _id } = product;
 
@@ -62,6 +69,15 @@ const SingleProduct = ({ product, onStarClick, star }) => {
         payload: true,
       });
     }
+  };
+
+  const handleAddToWishlist = (e) => {
+    e.preventDefault();
+    addToWishlist(product._id, user.token).then((res) => {
+      console.log("ADDED TO WISHLIST", res.data);
+      toast.success("Added to wishlist");
+      history.push("/user/wishlist");
+    });
   };
 
   return (
@@ -104,9 +120,9 @@ const SingleProduct = ({ product, onStarClick, star }) => {
               </a>
             </Tooltip>,
 
-            <Link to="/">
+            <a onClick={handleAddToWishlist}>
               <HeartOutlined className="text-info" /> <br /> Add to Wishlist
-            </Link>,
+            </a>,
 
             //aother rating popump using parent on star click here pass same to new child starrating do task there
             <RatingModal>
