845ffa163c7c109ab6f36c4b1b7c56f3bfe55fde
amitozs999 <ekas0001@gmail.com>
2023-11-11 18:42:14 +0530

    ui reload issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {},
+  },
   content: [
     "./public/**/*.{html,js,ts,jsx,tsx,mdx}",
     "./src/**/*.{html,js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{html,js,ts,jsx,tsx,mdx}",
     "./components/**/*.{html,js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
 };
