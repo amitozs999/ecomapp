48d5f4c1cf674a142aa7a203984211c6bfaa9b83
amitozs999 <ekas0001@gmail.com>
2023-11-15 15:11:53 +0530

    reponsive discount banner

--- src/components/banner/BannerBox.js
+++ src/components/banner/BannerBox.js
@@ -7,7 +7,7 @@ import DiscountCountdown from "../../components/discountCountdown/DiscountCountd
 //import laptop from "../../images/asas.jpg";
 import laptop2 from "./xx1.jpg";
 import laptop from "./axax.png";
-import "./index.css";
+import "./index.scss";
 import { useHistory } from "react-router-dom";
 const BannerBox = ({
   title,
@@ -33,7 +33,9 @@ const BannerBox = ({
   let x = id === "1" ? laptop : laptop2;
 
   return (
-    <div className="col-span-6 lg:col-span-6 flex  justify-center shadow-2xl relative rounded-lg overflow-hidden dark:bg-gray-500/70 !dark:bg-blend-multiply">
+    <div className="col-span-6 lg:col-span-6 flex  justify-center shadow-2xl relative rounded-lg overflow-hidden  ">
+      {/* <div className="bannertextdiv"></div> */}
+
       {/* //col-span-6 lg:col-span-6 flex  justify-center  shadow-2xl relative rounded-lg overflow-hidden dark:bg-gray-500/70 !dark:bg-blend-multiply */}
       <img
         width="200"
@@ -56,28 +58,27 @@ const BannerBox = ({
        
        sm:left-6         sm:w-[55%]  md:w-1/2       lg:w-[55%] xl:w-1/2"
       > */}
-      <div className="bannerdiv">
-        {/* <h3 className="float-right mr-6 font-mono text-white text-xl font-bold xxs:text-xs  sm:text-sm   md:text-2xl ltr:mr-4  sm:pt-8 lg:pt-2 xl:pt-8"> */}
+      {/* <div className="bannertextdiv"> */}
+      {/* <h3 className="float-right mr-6 font-mono text-white text-xl font-bold xxs:text-xs  sm:text-sm   md:text-2xl ltr:mr-4  sm:pt-8 lg:pt-2 xl:pt-8"> */}
 
-        {/* <h3 className="float-right   text-white     md:text-xl"> */}
+      {/* <h3 className="float-right   text-white     md:text-xl"> */}
 
-        <div className="textdiv">
-          <h3 className="flattext font-mono">FLAT {discount}% OFF</h3>
-          {/* <h3 className="float-right mr-6 font-serif text-white text-xl font-bold sm:text-xs/3 sm:font-normal md:text-xl   ltr:mr-4   ">
-           */}
-          <h3 className="codetext">Use Code : {codee}</h3>
+      <div className="textdiv">
+        <h3 className="flattext font-mono">FLAT {discount}% OFF</h3>
+        {/* <h3 className="float-right mr-6 font-serif text-white text-xl font-bold sm:text-xs/3 sm:font-normal md:text-xl   ltr:mr-4   ">
+         */}
+        <h3 className="codetext font-serif">Use Code : {codee}</h3>
 
-          <a
-            onClick={() => history.push(slug)}
-            //product/nike-mens-nike-air-max
-            // className="  py-2 px-3 sm:py-3 lg:py-2 xl:py-4 sm:px-4 rounded   float-right mr-3
-            //   transition-all duration-300 shadow-lg 2xl:mt-2 text-center zoommm2 inline-block  text-yellow-300 text-[12px] rtl:sm:text-xs   font-semibold"
+        <a
+          onClick={() => history.push(slug)}
+          //product/nike-mens-nike-air-max
+          // className="  py-2 px-3 sm:py-3 lg:py-2 xl:py-4 sm:px-4 rounded   float-right mr-3
+          //   transition-all duration-300 shadow-lg 2xl:mt-2 text-center zoommm2 inline-block  text-yellow-300 text-[12px] rtl:sm:text-xs   font-semibold"
 
-            className="shopbutton"
-          >
-            {buttonText}
-          </a>
-        </div>
+          className="shopbutton transition-all duration-300 shadow-lg zoommm2"
+        >
+          {buttonText}
+        </a>
       </div>
     </div>
   );
